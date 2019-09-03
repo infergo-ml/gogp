2b9fa15a8b301d2709c74fa04e9562952706d804
David Tolpin <david.tolpin@gmail.com>
2019-09-03 19:56:48 +0300

    comments with formulas with ll and log ll

--- gp/gp.go
+++ gp/gp.go
@@ -5,6 +5,8 @@ import (
 )
 
 type GP struct {
+	x      [][]float64
+	y      []float64
 	Kernel Model
 	Theta  []float64
 }
@@ -19,4 +21,14 @@ func (gp GP) Produce(x [][]float64) (mu, sigma []float64) {
 
 // Train optimizes hyperparameters given observations
 func (gp GP) Train(x [][]float64, y []float64) {
+}
+
+func (gp GP) ll() float64 {
+	// L = −½ log|Σ| − ½ (y−μ)^⊤ Σ^−1 (y−μ) − n/2 log(2π)
+	return 0.
+}
+
+func (gp GP) gradll() []float64 {
+	// ∇L = ½ tr(Σ^−1 ∂Σ/∂θ)􏰃+ ½ (y−μ)^⊤ ∂Σ/∂θ Σ^−1 ∂Σ/∂θ (y−μ)
+	return []float64{}
 }
