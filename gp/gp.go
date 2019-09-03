a65253c84da269099cba06597a8b834a4b5a1700
David Tolpin <david.tolpin@gmail.com>
2019-09-03 18:34:22 +0300

    functions

---
+++ gp/gp.go
@@ -0,0 +1,22 @@
+package gp
+
+import (
+	. "bitbucket/org/dtolpin/infergo/model"
+)
+
+type GP struct {
+	kernel Model
+	theta  []float64
+}
+
+// Absorb absorbs observations into the process
+func (gp GP) Absorb(x [][]float64, y []float64) {
+}
+
+// Produce computes predictions
+func (gp GP) Produce(x [][]float64) (mu, sigma []float64) {
+}
+
+// Train optimizes hyperparameters given observations
+func (gp GP) Train(x [][]float64, y []float64) {
+}
