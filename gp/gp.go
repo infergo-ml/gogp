4cac598ebc029d86a05e34fef7527cf86288d021
David Tolpin <david.tolpin@gmail.com>
2019-09-03 20:02:38 +0300

    stubs for ll; how do i turn this into a model?

--- gp/gp.go
+++ gp/gp.go
@@ -23,11 +23,14 @@ func (gp GP) Produce(x [][]float64) (mu, sigma []float64) {
 func (gp GP) Train(x [][]float64, y []float64) {
 }
 
+// ll computes log-likelihood of the parameters given the data
+// L = −½ log|Σ| − ½ (y−μ)^⊤ Σ^−1 (y−μ) − n/2 log(2π)
 func (gp GP) ll() float64 {
-	// L = −½ log|Σ| − ½ (y−μ)^⊤ Σ^−1 (y−μ) − n/2 log(2π)
 	return 0.
 }
 
+// gradll computes the gradient of the log-likelihood with respect
+// to the parameters and the input data locations
 func (gp GP) gradll() []float64 {
 	// ∇L = ½ tr(Σ^−1 ∂Σ/∂θ)􏰃+ ½ (y−μ)^⊤ ∂Σ/∂θ Σ^−1 ∂Σ/∂θ (y−μ)
 	return []float64{}
