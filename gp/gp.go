0c6097db0ab876ddefacfb04ec6ae709d4f25ca2
David Tolpin <david.tolpin@gmail.com>
2019-09-04 12:37:14 +0300

    drafting the structure of gp

--- gp/gp.go
+++ gp/gp.go
@@ -5,10 +5,9 @@ import (
 )
 
 type GP struct {
-	x      [][]float64
-	y      []float64
-	Kernel Model
-	Theta  []float64
+	NPar, NDim int       // dimensions
+	Kernel     Model     // kernel
+	Theta      []float64 // kernel parameters
 }
 
 // Absorb absorbs observations into the process
@@ -19,20 +18,20 @@ func (gp GP) Absorb(x [][]float64, y []float64) {
 func (gp GP) Produce(x [][]float64) (mu, sigma []float64) {
 }
 
-// Train optimizes hyperparameters given observations
-func (gp GP) Train(x [][]float64, y []float64) {
-}
+// Observe and Gradient implement Infergo's ElementalModel.
+// The model can be used, on its own or as a part of a larger
+// model, to infer the parameters.
 
-// ll computes log-likelihood of the parameters given the data
+// Observe computes log-likelihood of the parameters given the data
 // (GPML:5.8):
 //   L = −½ log|Σ| − ½ y^⊤ Σ^−1 y − n/2 log(2π)
-func (gp GP) ll() float64 {
+func (gp GP) Observe(x []float64) float64 {
 	return 0.
 }
 
 // gradll computes the gradient of the log-likelihood with respect
 // to the parameters and the input data locations (GPML:5.9):
 //   ∇L = ½ tr((α α^⊤ - Σ^−1) ∂Σ/∂θ), where α = Σ^-1 y
-func (gp GP) gradll() []float64 {
+func (gp GP) Gradient() []float64 {
 	return []float64{}
 }
