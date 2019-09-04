bece6b40d469d0f1263f76dbfa4b85c128af46e0
David Tolpin <david.tolpin@gmail.com>
2019-09-04 19:11:26 +0300

    absorb in progress

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module bitbucket.org/dtolpin/gogp
 
 go 1.12
+
+require gonum.org/v1/gonum v0.0.0-20190904110519-2065cbd6b42a
--- gp/gp.go
+++ gp/gp.go
@@ -2,16 +2,45 @@ package gp
 
 import (
 	. "bitbucket/org/dtolpin/infergo/model"
+	"gonum.org/v1/gonum/mat"
 )
 
 type GP struct {
-	NPar, NDim int       // dimensions
-	Kernel     Model     // kernel
-	Theta      []float64 // kernel parameters
+	NParam, NNoiseParam, NDim int       // dimensions
+	Kernel, NoiseKernel       Model     // kernel
+	Theta, ThetaNoise         []float64 // kernel parameters
 }
 
 // Absorb absorbs observations into the process
 func (gp GP) Absorb(x [][]float64, y []float64) {
+	// Covariance matrix
+	K := mat.NewSymDense(len(x), nil)
+	kargs := make([]float64, NParam+2*NDim)
+	nkargs := make([]float64, NNoiseParam+NDim)
+	copy(kargs, gp.Theta)
+	copy(nkargs, gp.ThetaNoise)
+	for i := 0; i != len(x); i++ {
+		copy(kargs[gp.NParam:], x[i])
+		copy(nkargs[gp.NNoiseParam:], x[i])
+
+		// Diagonal, includes the noise
+		copy(kargs[gp.NParam+gp.NDim:], x[i])
+		k := gp.Kernel.Observe(kargs)
+		n := gp.NoiseKernel.Observe(nkargs)
+		// TODO: gradient
+		K.Set(i, i, k+n)
+
+		// Off-diagonal, symmetric
+		for j := i + 1; i != len(x); j++ {
+			copy(kargs[gp.NParam+gp.NDim:], x[j])
+			k := gp.Kernel.Observe(kargs)
+			// TODO: gradient
+			K.SetSym(i, j, k)
+		}
+	}
+
+	var L mat.Cholesky
+	// TODO
 }
 
 // Produce computes predictions
