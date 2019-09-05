7f6b8930023f769b884567e7b5061f08f6047b20
David Tolpin <david.tolpin@gmail.com>
2019-09-05 09:09:00 +0300

    better docstrings

--- kernel/noise.go
+++ kernel/noise.go
@@ -6,15 +6,16 @@ import (
 
 // Noise kernels
 
-// ConstantNoise is a kernel given the same fixed noise for all
-// points. Used as a default when no noise kernel is given.
+// ConstantNoise is a noise kernel assigning the same fixed
+// noise to all points. Used as a default when no noise kernel
+// is given.
 type ConstantNoise float64
 func (nk ConstantNoise) Observe([]float64) float64 {
 	return float64(nk)
 }
 
-// UniformNoise is a kernel for learning the same noise variance
-// for all points.
+// UniformNoise is a noise kernel for learning the same noise
+// variance for all points.
 type UniformNoise struct {}
 func (nk UniformNoise) Observe (x []float64) float64 {
 	return math.Exp(x[0])
