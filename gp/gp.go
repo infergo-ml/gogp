fe7c9aa1fd44b2f65b412f09ccce5225e6661bce
David Tolpin <david.tolpin@gmail.com>
2019-09-10 15:47:16 +0300

    compiles, work in progress

--- gp/gp.go
+++ gp/gp.go
@@ -1,20 +1,23 @@
 package gp
 
 import (
-	. "bitbucket.org/dtolpin/infergo/model"
 	"bitbucket.org/dtolpin/gogp/kernel/ad"
+	. "bitbucket.org/dtolpin/infergo/model"
+	"fmt"
 	"gonum.org/v1/gonum/mat"
+	"math"
 )
 
 type GP struct {
-	NParam, NNoiseParam, NDim int  // dimensions
-	Kernel, NoiseKernel  Model     // kernel
-	Theta, NoiseTheta    []float64 // kernel parameters
-	Parallel             bool      // parallelize covariances
+	NParam, NNoiseParam, NDim int       // dimensions
+	Kernel, NoiseKernel       Model     // kernel
+	Theta, NoiseTheta         []float64 // kernel parameters
+	Parallel                  bool      // parallelize covariances
 
 	// Cached computations
-	l					 mat.Cholesky
-	alpha				 mat.VecDense
+	x	  [][]float64
+	l     mat.Cholesky
+	alpha *mat.VecDense
 }
 
 // Default noise, present for numerical stability; can
@@ -36,42 +39,44 @@ func (gp GP) Absorb(x [][]float64, y []float64) (err error) {
 
 	// Remember the input coordinates for computing covariances
 	// with predictions
-	gp.x := x
+	gp.x = x
 
 	// Covariance matrix
-	// TODO: compute the covariance matrix and gradient in parallel,
-	// i, j, kargs, nkargs must be copied for that
 	K := mat.NewSymDense(len(x), nil)
-	kargs := make([]float64, gp.NParam+2*gp.NDim)
-	nkargs := make([]float64, gp.NNoiseParam+gp.NDim)
-	copy(kargs, gp.Theta)
-	copy(nkargs, gp.NoiseTheta)
-	for i := 0; i != len(x); i++ {
-		copy(kargs[gp.NParam:], x[i])
-		copy(nkargs[gp.NNoiseParam:], x[i])
+	if gp.Parallel {
+		// TODO
+	} else {
+		kargs := make([]float64, gp.NParam+2*gp.NDim)
+		nkargs := make([]float64, gp.NNoiseParam+gp.NDim)
+		copy(kargs, gp.Theta)
+		copy(nkargs, gp.NoiseTheta)
+		for i := 0; i != len(x); i++ {
+			copy(kargs[gp.NParam:], x[i])
+			copy(nkargs[gp.NNoiseParam:], x[i])
 
-		// Diagonal, includes the noise
-		copy(kargs[gp.NParam+gp.NDim:], x[i])
-		k := gp.Kernel.Observe(kargs)
-		n := gp.NoiseKernel.Observe(nkargs)
-		// TODO: gradient
-		K.SetSym(i, i, k+n)
-
-		// Off-diagonal, symmetric
-		for j := i + 1; i != len(x); j++ {
-			copy(kargs[gp.NParam+gp.NDim:], x[j])
+			// Diagonal, includes the noise
+			copy(kargs[gp.NParam+gp.NDim:], x[i])
 			k := gp.Kernel.Observe(kargs)
+			n := gp.NoiseKernel.Observe(nkargs)
 			// TODO: gradient
-			K.SetSym(i, j, k)
+			K.SetSym(i, i, k+n)
+
+			// Off-diagonal, symmetric
+			for j := i + 1; i != len(x); j++ {
+				copy(kargs[gp.NParam+gp.NDim:], x[j])
+				k := gp.Kernel.Observe(kargs)
+				// TODO: gradient
+				K.SetSym(i, j, k)
+			}
 		}
 	}
 
 	if !gp.l.Factorize(K) {
 		return fmt.Errorf("factorize")
 	}
-	
-	gp.alpha := mat.NewVecDense(len(x), nil)
-	err := gp.l.SolveVecTo(gp.alpha, mat.NewVecDense(len(y), y))
+
+	gp.alpha = mat.NewVecDense(len(x), nil)
+	err = gp.l.SolveVecTo(gp.alpha, mat.NewVecDense(len(y), y))
 	if err != nil {
 		return err
 	}
@@ -80,25 +85,60 @@ func (gp GP) Absorb(x [][]float64, y []float64) (err error) {
 }
 
 // Produce computes predictions
-func (gp GP) Produce(x [][]float64) (mu, sigma []float64) {
+func (gp GP) Produce(x [][]float64) (
+	mu, sigma []float64,
+	err error,
+) {
 	Kstar := mat.NewDense(len(gp.x), len(x), nil)
-	// TODO: parallel
-	kargs := make([]float64, gp.NParam+2*gp.NDim)
-	copy(kargs, gp.Theta)
-	for i := 0; i != len(gp.x); i++ {
-		copy(kargs[gp.NParam:], gp.x[i])
-		for j := 0; i != len(x); j++ {
-			copy(kargs[gp.NParam+gp.NDim:], x[j])
-			k := gp.Kernel.Observe(kargs)
-			// TODO: drop gradient
-			Kstar.Set(i, j, k)
+	if gp.Parallel {
+		//TODO
+	} else {
+		kargs := make([]float64, gp.NParam+2*gp.NDim)
+		copy(kargs, gp.Theta)
+		for i := 0; i != len(gp.x); i++ {
+			copy(kargs[gp.NParam:], gp.x[i])
+			for j := 0; i != len(x); j++ {
+				copy(kargs[gp.NParam+gp.NDim:], x[j])
+				k := gp.Kernel.Observe(kargs)
+				// TODO: drop gradient
+				Kstar.Set(i, j, k)
+			}
 		}
 	}
 
 	var mean mat.VecDense
 	mean.MulVec(Kstar, gp.alpha)
+	mu = mean.RawVector().Data
 
-	return mean.RawVector().Data, 
+	variance := mat.NewVecDense(len(x), nil)
+	if gp.Parallel {
+		// TODO
+	} else {
+		kargs := make([]float64, gp.NParam+2*gp.NDim)
+		copy(kargs, gp.Theta)
+		for i := 0; i != len(x); i++ {
+			copy(kargs[gp.NParam:], gp.x[i])
+			copy(kargs[gp.NParam+gp.NDim:], gp.x[i])
+			k := gp.Kernel.Observe(kargs)
+			// TODO: drop gradient
+			variance.SetVec(i, k)
+		}
+	}
+
+	v := mat.NewDense(len(gp.x), len(x), nil)
+	if err := gp.l.SolveTo(v, Kstar); err != nil {
+		return nil, nil, err
+	}
+	covariance := mat.NewDense(len(x), len(x), nil)
+	covariance.Mul(Kstar.T(), v)
+
+	// TODO: find an idiomatic way to write this
+	sigma = variance.RawVector().Data
+	for i := range sigma {
+		sigma[i] = math.Sqrt(sigma[i] - covariance.At(i, i))
+	}
+
+	return mu, sigma, nil
 }
 
 // Observe and Gradient implement Infergo's ElementalModel.
