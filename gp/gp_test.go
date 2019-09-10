f117086054593e4eab21427ed460cf497c16af36
David Tolpin <david.tolpin@gmail.com>
2019-09-10 20:47:44 +0300

    reformatted

--- gp/gp_test.go
+++ gp/gp_test.go
@@ -7,102 +7,102 @@ import (
 )
 
 func TestProduce(t *testing.T) {
-	for _, c := range [] struct {
-		name string
-		gp *GP
-		x [][]float64
-		y []float64
-		z [][]float64
+	for _, c := range []struct {
+		name      string
+		gp        *GP
+		x         [][]float64
+		y         []float64
+		z         [][]float64
 		mu, sigma []float64
-	} {
+	}{
 		{
 			name: "prior",
-			gp: &GP {
-				NParam: 1,
-				NDim: 1,
-				Kernel: kernel.Normal,
+			gp: &GP{
+				NParam:      1,
+				NDim:        1,
+				Kernel:      kernel.Normal,
 				NoiseKernel: kernel.ConstantNoise(0),
-				Theta: []float64{1.},
+				Theta:       []float64{1.},
 			},
-			x: [][]float64{},
-			y: []float64{},
-			z: [][]float64{{0}},
-			mu: []float64{0},
+			x:     [][]float64{},
+			y:     []float64{},
+			z:     [][]float64{{0}},
+			mu:    []float64{0},
 			sigma: []float64{1},
 		},
 		{
 			name: "1 self",
-			gp: &GP {
-				NParam: 1,
-				NDim: 1,
-				Kernel: kernel.Normal,
+			gp: &GP{
+				NParam:      1,
+				NDim:        1,
+				Kernel:      kernel.Normal,
 				NoiseKernel: kernel.ConstantNoise(0),
-				Theta: []float64{1.},
+				Theta:       []float64{1.},
 			},
-			x: [][]float64{{0}},
-			y: []float64{1},
-			z: [][]float64{{0}},
-			mu: []float64{1},
+			x:     [][]float64{{0}},
+			y:     []float64{1},
+			z:     [][]float64{{0}},
+			mu:    []float64{1},
 			sigma: []float64{0},
 		},
 		{
 			name: "two selves",
-			gp: &GP {
-				NParam: 1,
-				NDim: 1,
-				Kernel: kernel.Normal,
+			gp: &GP{
+				NParam:      1,
+				NDim:        1,
+				Kernel:      kernel.Normal,
 				NoiseKernel: kernel.ConstantNoise(0),
-				Theta: []float64{1.},
+				Theta:       []float64{1.},
 			},
-			x: [][]float64{{0}, {1}},
-			y: []float64{1, -1},
-			z: [][]float64{{0}, {1}},
-			mu: []float64{1, -1},
+			x:     [][]float64{{0}, {1}},
+			y:     []float64{1, -1},
+			z:     [][]float64{{0}, {1}},
+			mu:    []float64{1, -1},
 			sigma: []float64{0, 0},
 		},
 		{
 			name: "inter",
-			gp: &GP {
-				NParam: 1,
-				NDim: 1,
-				Kernel: kernel.Normal,
+			gp: &GP{
+				NParam:      1,
+				NDim:        1,
+				Kernel:      kernel.Normal,
 				NoiseKernel: kernel.ConstantNoise(0),
-				Theta: []float64{1.},
+				Theta:       []float64{1.},
 			},
-			x: [][]float64{{0}, {1}},
-			y: []float64{1, -1},
-			z: [][]float64{{0.5}},
-			mu: []float64{0.},
+			x:     [][]float64{{0}, {1}},
+			y:     []float64{1, -1},
+			z:     [][]float64{{0.5}},
+			mu:    []float64{0.},
 			sigma: []float64{0.174518},
 		},
 		{
 			name: "extra",
-			gp: &GP {
-				NParam: 1,
-				NDim: 1,
-				Kernel: kernel.Normal,
+			gp: &GP{
+				NParam:      1,
+				NDim:        1,
+				Kernel:      kernel.Normal,
 				NoiseKernel: kernel.ConstantNoise(0),
-				Theta: []float64{1.},
+				Theta:       []float64{1.},
 			},
-			x: [][]float64{{0}, {1}},
-			y: []float64{1, -1},
-			z: [][]float64{{-2.}, {3.}},
-			mu: []float64{0.315720, -0.315720},
+			x:     [][]float64{{0}, {1}},
+			y:     []float64{1, -1},
+			z:     [][]float64{{-2.}, {3.}},
+			mu:    []float64{0.315720, -0.315720},
 			sigma: []float64{0.986770, 0.986770},
 		},
 		{
 			name: "noise",
-			gp: &GP {
-				NParam: 1,
-				NDim: 1,
-				Kernel: kernel.Normal,
+			gp: &GP{
+				NParam:      1,
+				NDim:        1,
+				Kernel:      kernel.Normal,
 				NoiseKernel: kernel.ConstantNoise(0.1),
-				Theta: []float64{1.},
+				Theta:       []float64{1.},
 			},
-			x: [][]float64{{0}, {1}},
-			y: []float64{1, -1},
-			z: [][]float64{{-2.}, {3.}},
-			mu: []float64{0.251741, -0.251741},
+			x:     [][]float64{{0}, {1}},
+			y:     []float64{1, -1},
+			z:     [][]float64{{-2.}, {3.}},
+			mu:    []float64{0.251741, -0.251741},
 			sigma: []float64{0.988979, 0.988979},
 		},
 	} {
@@ -123,17 +123,17 @@ func TestProduce(t *testing.T) {
 				c.name, len(sigma), len(c.sigma))
 		}
 		for i := range mu {
-			if math.Abs(mu[i] - c.mu[i]) > 1E-6 {
+			if math.Abs(mu[i]-c.mu[i]) > 1E-6 {
 				t.Errorf("%s: wrong mu: got %v, want %v",
-						 c.name, mu, c.mu)
-			    break
+					c.name, mu, c.mu)
+				break
 			}
 		}
 		for i := range sigma {
-			if math.Abs(sigma[i] - c.sigma[i]) > 1E-6 {
+			if math.Abs(sigma[i]-c.sigma[i]) > 1E-6 {
 				t.Errorf("%s: wrong sigma: got %v, want %v",
-						 c.name, sigma, c.sigma)
-			    break
+					c.name, sigma, c.sigma)
+				break
 			}
 		}
 	}
