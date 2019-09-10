f9555f2f07cdbf41769eb9952dd02fd73a1a4de8
David Tolpin <david.tolpin@gmail.com>
2019-09-10 13:45:06 +0300

    in progress

--- gp/gp.go
+++ gp/gp.go
@@ -10,6 +10,11 @@ type GP struct {
 	NParam, NNoiseParam, NDim int  // dimensions
 	Kernel, NoiseKernel  Model     // kernel
 	Theta, NoiseTheta    []float64 // kernel parameters
+	Parallel             bool      // parallelize covariances
+
+	// Cached computations
+	l					 mat.Cholesky
+	alpha				 mat.VecDense
 }
 
 // Default noise, present for numerical stability; can
@@ -25,9 +30,13 @@ func (gp GP) defaults() {
 }
 
 // Absorb absorbs observations into the process
-func (gp GP) Absorb(x [][]float64, y []float64) {
+func (gp GP) Absorb(x [][]float64, y []float64) (err error) {
 	// Set the defaults
 	gp.defaults()
+
+	// Remember the input coordinates for computing covariances
+	// with predictions
+	gp.x := x
 
 	// Covariance matrix
 	// TODO: compute the covariance matrix and gradient in parallel,
@@ -57,15 +66,39 @@ func (gp GP) Absorb(x [][]float64, y []float64) {
 		}
 	}
 
-	var L mat.Cholesky
-	L.Factorize(K)
-	// TODO
+	if !gp.l.Factorize(K) {
+		return fmt.Errorf("factorize")
+	}
+	
+	gp.alpha := mat.NewVecDense(len(x), nil)
+	err := gp.l.SolveVecTo(gp.alpha, mat.NewVecDense(len(y), y))
+	if err != nil {
+		return err
+	}
 
+	return
 }
 
 // Produce computes predictions
 func (gp GP) Produce(x [][]float64) (mu, sigma []float64) {
-	return
+	Kstar := mat.NewDense(len(gp.x), len(x), nil)
+	// TODO: parallel
+	kargs := make([]float64, gp.NParam+2*gp.NDim)
+	copy(kargs, gp.Theta)
+	for i := 0; i != len(gp.x); i++ {
+		copy(kargs[gp.NParam:], gp.x[i])
+		for j := 0; i != len(x); j++ {
+			copy(kargs[gp.NParam+gp.NDim:], x[j])
+			k := gp.Kernel.Observe(kargs)
+			// TODO: drop gradient
+			Kstar.Set(i, j, k)
+		}
+	}
+
+	var mean mat.VecDense
+	mean.MulVec(Kstar, gp.alpha)
+
+	return mean.RawVector().Data, 
 }
 
 // Observe and Gradient implement Infergo's ElementalModel.
