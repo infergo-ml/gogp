1a42788ce5a1860af7392a918479b923654921d2
David Tolpin <david.tolpin@gmail.com>
2019-09-28 17:53:36 +0300

    skeleton for warpedtime

---
+++ tutorial/warpedtime/kernel/kernel.go
@@ -0,0 +1,38 @@
+package kernel
+
+import (
+	"bitbucket.org/dtolpin/gogp/kernel"
+)
+
+// The similarity kernel.
+type simil struct{}
+
+var Simil simil
+
+func (simil) Observe(x []float64) float64 {
+	const (
+		c1 = iota // trend scale
+		c2        // season scale
+		l1        // trend length scale
+		l2        // season length scale
+		p         // season period
+		xa        // first point
+		xb        // second point
+	)
+
+	return x[c1]*kernel.Matern52.Cov(x[l1], x[xa], x[xb]) +
+		x[c2]*kernel.Periodic.Cov(x[l2], 10*x[p], x[xa], x[xb])
+}
+
+func (simil) NTheta() int { return 5 }
+
+// The noise kernel.
+type noise struct{}
+
+var Noise noise
+
+func (n noise) Observe(x []float64) float64 {
+	return 0.01 * kernel.UniformNoise.Observe(x)
+}
+
+func (noise) NTheta() int { return 1 }
