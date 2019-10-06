ba29442b510e0dc6dc73defab2bdd5ca3863861c
David Tolpin <david.tolpin@gmail.com>
2019-10-06 06:51:49 +0300

    added forgotten

---
+++ tutorial/anynoise/kernel/kernel.go
@@ -0,0 +1,34 @@
+package kernel
+
+import (
+	"bitbucket.org/dtolpin/gogp/kernel"
+)
+
+// The similarity kernel.
+type simil struct{}
+
+var Simil simil
+
+func (simil) Observe(x []float64) float64 {
+	const (
+		c  = iota // output scale
+		l         // length scale
+		xa        // first point
+		xb        // second point
+	)
+
+	return x[c] * kernel.Matern52.Cov(x[l], x[xa], x[xb])
+}
+
+func (simil) NTheta() int { return 2 }
+
+// The noise kernel.
+type noise struct{}
+
+var Noise noise
+
+func (n noise) Observe(x []float64) float64 {
+	return 0.01 * kernel.UniformNoise.Observe(x)
+}
+
+func (noise) NTheta() int { return 1 }
---
+++ tutorial/anynoise/model/model.go
@@ -0,0 +1,45 @@
+package model
+
+import (
+	. "bitbucket.org/dtolpin/infergo/dist"
+	"math"
+)
+
+type Priors struct {
+	Y []float64 // noisy outputs
+}
+
+func (m *Priors) Observe(x []float64) float64 {
+	const (
+		s  = iota // noise
+		c         // output scale
+		l         // length scale
+		i0        // first input
+	)
+
+	n := len(x[i0:]) / 2
+	if len(m.Y) != n {
+		// First call, memoize initial distances between inputs
+		m.Y = make([]float64, n)
+		copy(m.Y, x[i0+n:])
+	}
+
+	ll := 0.
+
+	// The noise is scaled by 0.1.
+	ll += Normal.Logp(0, 1, x[s])
+
+	// Output scale is mostly less than 1.
+	ll += Normal.Logp(-1, 1, x[c])
+
+	// Length scale is around 1, in wide margins.
+	ll += Normal.Logp(0, 2, x[l])
+
+	// Instead of Gaussian, we assume Laplacian noise.
+	for i := range m.Y {
+		ll += Expon.Logp(1/(0.1*math.Exp(x[s])),
+			math.Abs(m.Y[i]-x[i0+n+i]))
+	}
+
+	return ll
+}
